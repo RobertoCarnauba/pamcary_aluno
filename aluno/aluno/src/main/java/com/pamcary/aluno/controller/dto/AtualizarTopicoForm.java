c1f563b32aa3d366dc5b39e3afd6885bd3875c24
Roberto Carnaúba Dias <betocarnauba@hotmail.com>
2020-05-28 16:40:25 -0300

    up
    update

---
+++ aluno/aluno/src/main/java/com/pamcary/aluno/controller/dto/AtualizarTopicoForm.java
@@ -0,0 +1,45 @@
+package com.pamcary.aluno.controller.dto;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.pamcary.aluno.model.Topico;
+import com.pamcary.aluno.repository.TopicoRepository;
+
+public class AtualizarTopicoForm {
+
+	@NotNull @NotEmpty @Length(min = 5)
+	private String titulo;
+	@NotNull @NotEmpty @Length(min = 5)
+	private String mensagem;
+	
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+	public Topico atualizar(Long id, TopicoRepository topicoRepository) {
+		System.out.println("Chegamosa qui");
+		Topico topico = topicoRepository.getOne(id);
+		topico.setTitulo(titulo);
+		topico.setMensagem(mensagem);
+		return topico;
+	}
+	
+	
+	
+
+	
+
+}
